cef5faf73777d7add53e59c6053fe9c48594e2d9
pn481 <pntandane72@gmail.com>
2025-04-28 13:14:40 -0700

    commit main

---
+++ script.js
@@ -0,0 +1,6 @@
+
+// Mobile menu toggle const burger = document.querySelector('.burger'); const nav = document.querySelector('.nav-links');
+
+burger.addEventListener('click', () => { nav.classList.toggle('nav-active');
+
+    // Burger animation burger.classList.toggle('toggle'); });
